9177a3e5ace2ca3fcf24e506f719585c8853d5a8
Phil <phil.board90@googlemail.com>
2019-04-17 14:46:10 +0100

    you can increase temp

--- jasmine-standalone-3.4.0/src/Thermostat.js
+++ jasmine-standalone-3.4.0/src/Thermostat.js
@@ -1,4 +1,13 @@
 'use strict';
-function Thermostat(){
+
+function Thermostat() {
   this.temperature = 20;
+}
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function(){
+  this.temperature +=1;
 };
