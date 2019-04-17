9e2ef857722291fed914c8324dba5bb9af8cc553
Phil <phil.board90@googlemail.com>
2019-04-17 15:38:05 +0100

    added new max temp to tie in with power saving mode

--- jasmine-standalone-3.4.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.4.0/spec/ThermostatSpec.js
@@ -37,4 +37,27 @@ describe('Thermostat', function() {
     thermostat.switchPowerSavingModeOff();
     expect(thermostat.isPowerSavingModeOn()).toBe(false);
   });
+
+  it('can switch PSM back on', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+});
+
+describe('when power saving mode is on', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('has a maximum temperature of 25 degrees', function() {
+    for (var temp = 20; temp < 25; temp++) {
+      thermostat.up();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(25);
+  });
 });
